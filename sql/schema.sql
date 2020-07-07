cc16db282ecbf6f5821f3991f77dcb3688d0ba58
Drew Gochenaur <drew@ariel.lan>
2020-07-07 06:48:31 -0700

    dnstap

--- sql/schema.sql
+++ sql/schema.sql
@@ -3,9 +3,13 @@ CREATE DATABASE IF NOT EXISTS ipmonitor;
 USE ipmonitor;
 
 CREATE TABLE IF NOT EXISTS ips (
-    ip VARCHAR(64) NOT NULL PRIMARY KEY,
+    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    tble VARCHAR(16) NOT NULL,
+    ip VARCHAR(64) NOT NULL,
     ttl BIGINT NOT NULL
 );
 
 CREATE USER IF NOT EXISTS 'ipmonitor'@'%' IDENTIFIED WITH mysql_native_password BY 'rotinompi';
 GRANT SELECT, INSERT ON ipmonitor.ips TO 'ipmonitor'@'%';
+
+CREATE INDEX tble_ip ON ips (tble, ip);
